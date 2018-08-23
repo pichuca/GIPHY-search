3f327842291334160dafcc2b2298d6ade0611d78
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-08-22 22:40:09 -0300

    Fixed merge conflicts.

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # GIPHY-search
 search GIFs using Giphy API
 
-npm start
+make sure you have installed Nodejs from: https://nodejs.org/en/download/
+
+Application runs with:
+npm 6.4.0
+node v8.9.1
+
+go to dir: /app
+
+and:
+
+run `npm start`
     Starts the development server.
 
-npm run build   
+run `npm run build`   
     Bundles the app into static files for production.
 
-npm test
+run `npm test`
     Starts the test runner.
-
--- app/src/App.js
+++ app/src/App.js
@@ -7,19 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
           <p className="App-intro">
             <code>GIPHY-search</code>
           </p>
         </header>
-=======
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
->>>>>>> 2ca32c35a2db5b57f7fbfaf18ad346646ebaf325
       </div>
     );
   }
