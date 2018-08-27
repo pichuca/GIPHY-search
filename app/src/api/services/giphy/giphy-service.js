af4a67972f8fdb4414aefa5caacd85d8308d548a
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-08-26 23:44:32 -0300

    Start directory structure. Added service layer to get data.

---
+++ app/src/api/services/giphy/giphy-service.js
@@ -0,0 +1,14 @@
+/**
+ * GIPHY service.
+ * Get data from GIPHY API.
+ */
+
+class GiphyService {
+  static getTrending() {
+    // TODO: trending
+  }
+
+  static searchQuery(query) {
+    // TODO: search
+  }
+}
