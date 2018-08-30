e38b77683f62c6b3f253b0d1c60fa8cdd0306303
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-08-29 22:34:22 -0300

    Test router.

--- app/src/App.js
+++ app/src/App.js
@@ -7,23 +7,22 @@ import {
 
 import './App.css';
 import SearchAndTrending from './features/search-and-trending/SearchAndTrending';
+import SearchResults from './features/search-results/SearchResults';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
-          <p className="app-intro">
-            <code>GIPHY-search</code>
-          </p>
-        </header>
-        <Router>
-          <SearchAndTrending />
-          
-
-          <Route path="/search-results" component={}/>
-        </Router>
-      </div>
+      <Router>
+        <div className="app">
+          <header className="app-header">
+            <p className="app-intro">
+              <code>GIPHY-search</code>
+            </p>
+          </header>
+          <Route exact path="/" component={SearchAndTrending} />
+          <Route path="/search-results" component={SearchResults}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ app/src/features/search-results/SearchResults.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="search-results">
+        <h1>Search results</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
